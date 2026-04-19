3bfc20763a8b7d0d5678fd76575bb0a1945eed2a
rodrigofaustinoo-pixel <rodrigofaustinoo@gmail.com>
2026-04-19 00:37:45 -0300

    Create UsuarioRepository.java

---
+++ UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.farmacia.repository;
+
+import com.farmacia.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Usuario findByLogin(String login);
+}
