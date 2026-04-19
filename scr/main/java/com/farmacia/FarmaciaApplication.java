4e64229430113f4f25eb4567bad6a688c1543536
rodrigofaustinoo-pixel <rodrigofaustinoo@gmail.com>
2026-04-18 23:06:21 -0300

    Update FarmaciaApplication.java

--- scr/main/java/com/farmacia/FarmaciaApplication.java
+++ scr/main/java/com/farmacia/FarmaciaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication 
   public class FarmaciaApplication {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
       SpringApplication.run(FarmaciaApplication.class, args);
     }
   }
