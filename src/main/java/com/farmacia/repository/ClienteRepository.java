2392179db44023b8bc353b9ace421952b2688c6e
rodrigofaustinoo-pixel <rodrigofaustinoo@gmail.com>
2026-04-19 03:14:10 -0300

    Delete src/main/java/com/farmacia/repository directory

--- src/main/java/com/farmacia/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.farmacia.repository;
-
-import com.farmacia.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-    Cliente findByCpf(String cpf);
-}
--- src/main/java/com/farmacia/repository/UsuarioReposotory.java
+++
@@ -1,8 +0,0 @@
-package com.farmacia.repository;
-
-import com.farmacia.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByLogin(String login);
-}
