e8761cad70ef5a4b7cdf02d4aa237ee1e82afdfb
rodrigofaustinoo-pixel <rodrigofaustinoo@gmail.com>
2026-04-19 11:29:28 -0300

    Create Cliente.java

---
+++ src/main/java/com/farmacia/model/Cliente.java
@@ -0,0 +1,22 @@
+package com.farmacia.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String nome;
+    private String cpf;
+
+    public Long getId() { return id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+}
